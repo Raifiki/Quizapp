64d4249ed91baca7443c2acea9f60d0feb48a04c
Raifikie <leonard_weiss@web.dem>
2023-04-21 19:44:56 +0200

    Funktion von Start Btn hinzugefügt, Design responsive und feinschliff

--- code/script.js
+++ code/script.js
@@ -1,136 +1,175 @@
 let allCatagories = [
-    [    {
-            "question": "Frage 1",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 1
+    [
+        {
+        "question": "Was bedeutet HTML?",
+        "answer_1": "High Text Multi Layer",
+        "answer_2": "Hyper Tool Multilingual",
+        "answer_3": "Hypertech Mainframe Language",
+        "answer_4": "Hypertext Markup Language",
+        "right_answer": 4
         },
         {
-            "question": "Frage 2",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 2
+        "question": "Welches Tag wird verwendet, um eine Überschrift erster Ordnung zu definieren?",
+        "answer_1": "&lt;h2&gt;",
+        "answer_2": "&lt;h1&gt;",
+        "answer_3": "&lt;h3&gt;",
+        "answer_4": "&lt;h4&gt;",
+        "right_answer": 2
         },
         {
-            "question": "Frage 3",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 3
+        "question": "Was ist die korrekte Syntax für das Hinzufügen eines Links in HTML?",
+        "answer_1": "&lt;link url='http://example.com'&gt;Link&lt;/link&gt;",
+        "answer_2": "&lt;a url='http://example.com'&gt;Link&lt;/a&gt;",
+        "answer_3": "&lt;a href='http://example.com'&gt;Link&lt;/a&gt;",
+        "answer_4": "&lt;link href='http://example.com'&gt;Link&lt;/link&gt;",
+        "right_answer": 3
         },
         {
-            "question": "Frage 4",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 4
+        "question": "Welches Tag wird verwendet, um eine Liste mit Nummern zu erstellen?",
+        "answer_1": "&lt;ul&gt;",
+        "answer_2": "&lt;dl&gt;",
+        "answer_3": "&lt;ol&gt;",
+        "answer_4": "&lt;li&gt;",
+        "right_answer": 3
+        },
+        {
+        "question": "Was ist der Zweck des 'alt'-Attributs in einem Bild-Tag?",
+        "answer_1": "Es gibt an, wo sich das Bild befindet.",
+        "answer_2": "Es gibt an, wie breit das Bild sein sollte.",
+        "answer_3": "Es gibt an, welcher Text angezeigt wird, wenn man mit der Maus über das Bild fährt.",
+        "answer_4": "Es gibt an, welcher Text angezeigt wird, wenn das Bild nicht geladen werden kann.",
+        "right_answer": 4
         }
     ],
-    [    {
-            "question": "Frage 11",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 1
+      [
+        {
+          "question": "Was ist CSS?",
+          "answer_1": "Computer System Services",
+          "answer_2": "Cascading Style Sheets",
+          "answer_3": "Controlled Style Syntax",
+          "answer_4": "Computer Style Script",
+          "right_answer": 2
         },
         {
-            "question": "Frage 12",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 2
+          "question": "Was ist der Zweck von CSS?",
+          "answer_1": "Zur Verwaltung von Datenbanken",
+          "answer_2": "Zur Gestaltung von Webseiten",
+          "answer_3": "Zur Erstellung von Videospielen",
+          "answer_4": "Zur Programmierung von Apps",
+          "right_answer": 2
         },
         {
-            "question": "Frage 13",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 3
+          "question": "Welches Attribut wird verwendet, um eine Hintergrundfarbe in CSS festzulegen?",
+          "answer_1": "background-color",
+          "answer_2": "color",
+          "answer_3": "border",
+          "answer_4": "font-size",
+          "right_answer": 1
         },
         {
-            "question": "Frage 14",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 4
+          "question": "Wie kann man in CSS eine ID-Selektor definieren?",
+          "answer_1": "Durch ein '#' gefolgt von einem Attribut",
+          "answer_2": "Durch ein '.' gefolgt von einem Attribut",
+          "answer_3": "Durch ein '+' gefolgt von einem Attribut",
+          "answer_4": "Durch ein '&' gefolgt von einem Attribut",
+          "right_answer": 1
+        },
+        {
+          "question": "Welches Attribut wird verwendet, um die Größe von Schriftarten in CSS zu ändern?",
+          "answer_1": "text-size",
+          "answer_2": "font-size",
+          "answer_3": "size",
+          "answer_4": "font-style",
+          "right_answer": 2
         }
-    ],
-    [    {
-            "question": "Frage 21",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 1
+      ]
+      ,
+      [
+        {
+          "question": "Was ist JavaScript?",
+          "answer_1": "Eine Programmiersprache für das Web",
+          "answer_2": "Ein Texteditor für HTML- und CSS-Dateien",
+          "answer_3": "Ein Framework für das Erstellen von Apps",
+          "answer_4": "Eine Datenbank für Webanwendungen",
+          "right_answer": 1
         },
         {
-            "question": "Frage 22",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 2
+          "question": "Was ist die Verwendung von Variablen in JavaScript?",
+          "answer_1": "Um Elemente auf der Webseite zu manipulieren",
+          "answer_2": "Um Bedingungen und Schleifen zu steuern",
+          "answer_3": "Um Funktionen zu definieren",
+          "answer_4": "Um Datentypen zu speichern",
+          "right_answer": 4
         },
         {
-            "question": "Frage 23",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 3
+          "question": "Was ist ein Array in JavaScript?",
+          "answer_1": "Ein Objekt, das eine Sammlung von Elementen enthält",
+          "answer_2": "Ein Typ von Variablen, die nur Boolean-Werte speichern können",
+          "answer_3": "Ein Datentyp, der nur für Strings verwendet wird",
+          "answer_4": "Ein spezielles Element, das nur für HTML-Formulare verwendet wird",
+          "right_answer": 1
         },
         {
-            "question": "Frage 24",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 4
+          "question": "Was ist der Unterschied zwischen == und === in JavaScript?",
+          "answer_1": "== prüft nur auf Gleichheit des Werts, === prüft auf Gleichheit des Werts und Datentyps",
+          "answer_2": "== prüft auf Gleichheit des Werts und Datentyps, === prüft nur auf Gleichheit des Werts",
+          "answer_3": "== prüft auf Unterschiedlichkeit des Werts, === prüft auf Unterschiedlichkeit des Werts und Datentyps",
+          "answer_4": "== und === sind in JavaScript synonym und können austauschbar verwendet werden",
+          "right_answer": 1
+        },
+        {
+          "question": "Was ist eine Funktion in JavaScript?",
+          "answer_1": "Eine Möglichkeit, auf eine externe Bibliothek zuzugreifen",
+          "answer_2": "Ein Code-Block, der eine bestimmte Aufgabe ausführt, wenn er aufgerufen wird",
+          "answer_3": "Eine Anweisung, die eine Schleife innerhalb einer anderen Schleife beendet",
+          "answer_4": "Ein Objekt, das eine Sammlung von Schlüssel-Wert-Paaren enthält",
+          "right_answer": 2
         }
-    ],    
-    [    {
-            "question": "Frage 31",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 1
+      ]
+      ,    
+      [
+        {
+          "question": "Was ist Java?",
+          "answer_1": "Eine Betriebssystemfamilie",
+          "answer_2": "Ein Texteditor für Code-Dateien",
+          "answer_3": "Eine Programmiersprache",
+          "answer_4": "Ein Framework für Webanwendungen",
+          "right_answer": 3
         },
         {
-            "question": "Frage 32",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 2
+          "question": "Was ist das Konzept der Vererbung in Java?",
+          "answer_1": "Das Erstellen von Objekten aus Klassen",
+          "answer_2": "Die Möglichkeit, Code aus einer Klasse in eine andere Klasse zu übernehmen",
+          "answer_3": "Die Fähigkeit, Daten zwischen Objekten auszutauschen",
+          "answer_4": "Die Verwendung von Interfaces zur Definition von Methoden",
+          "right_answer": 2
         },
         {
-            "question": "Frage 33",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 3
+          "question": "Was ist eine Ausnahme in Java?",
+          "answer_1": "Ein Fehler, der während der Kompilierung auftritt",
+          "answer_2": "Ein Fehler, der während der Laufzeit auftritt",
+          "answer_3": "Ein Warnhinweis, der während der Ausführung angezeigt wird",
+          "answer_4": "Eine Methode, die den Programmablauf unterbricht",
+          "right_answer": 2
         },
         {
-            "question": "Frage 34",
-            "answer_1": "Antwortmöglichkeit 1",
-            "answer_2": "Antwortmöglichkeit 2",
-            "answer_3": "Antwortmöglichkeit 3",
-            "answer_4": "Antwortmöglichkeit 4",
-            "right_answer": 4
+          "question": "Was ist das Konzept der Polymorphie in Java?",
+          "answer_1": "Die Möglichkeit, eine Methode mit demselben Namen in verschiedenen Klassen zu definieren",
+          "answer_2": "Die Möglichkeit, eine Klasse aus einer anderen Klasse zu erben",
+          "answer_3": "Die Fähigkeit, mehrere Methoden in einer Klasse zu definieren",
+          "answer_4": "Die Möglichkeit, eine Variable mit mehreren Werten zu initialisieren",
+          "right_answer": 1
+        },
+        {
+          "question": "Was ist eine Schnittstelle in Java?",
+          "answer_1": "Eine Klasse, die von einer anderen Klasse erbt",
+          "answer_2": "Eine Methode, die in einer anderen Klasse definiert ist",
+          "answer_3": "Eine Möglichkeit, Code in einer Klasse zu organisieren",
+          "answer_4": "Ein Vertrag, der eine bestimmte Menge von Methoden definiert, die eine Klasse implementieren muss",
+          "right_answer": 4
         }
-    ]      
+      ]
+            
 ];
 let currentCategorie = 0;
 let questions = allCatagories[currentCategorie];
